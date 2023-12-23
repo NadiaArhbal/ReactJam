6ab57f71ffe842c61f6c5450739012e27d3ac748
NadiaArhbal <54160@etu.he2b.be>
2023-12-23 21:14:04 +0100

    Affichage de la foret

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import { useEffect, useState } from "react"
 import reactLogo from "./assets/rune.svg"
+import React, { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import viteLogo from "/vite.svg"
+import { OrbitControls, useGLTF } from "@react-three/drei"
 import "./App.css"
+
+
+function Box(props) {
+  const meshRef = useRef()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 function App() {
   const [game, setGame] = useState()
@@ -17,9 +40,11 @@ function App() {
     return <div>Loading...</div>
   }
 
+  const {scene} = useGLTF("foretfinal.glb");
+
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,15 +56,17 @@ function App() {
       <div className="card">
         <button onClick={() => Rune.actions.increment({ amount: 1 })}>
           count is {game.count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> or <code>src/logic.ts</code> and save to
-          test HMR
-        </p>
-      </div>
+        </button> */}
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <primitive object={scene} />
+          <OrbitControls />
+        </Canvas>,
+      {/* </div>
       <p className="read-the-docs">
         Click on the Vite and Rune logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
