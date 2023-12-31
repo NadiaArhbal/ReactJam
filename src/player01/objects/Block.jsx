1d5b88813e9c86038260f9b976a52fc7ee0d9dc5
NadiaArhbal <54160@etu.he2b.be>
2023-12-31 18:36:45 +0100

    Color problem

--- src/player01/objects/Block.jsx
+++ src/player01/objects/Block.jsx
@@ -2,24 +2,10 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Block(props) {
-    const { nodes, materials } = useGLTF("/blocks/block01.glb");
+    const { nodes, materials } = useGLTF("/blocks/Block01.glb");
     return (
-        <group {...props} dispose={null}>
-        <mesh
-            geometry={nodes.Block01.geometry}
-            material={materials.Yellow}
-        />
-        <mesh
-            geometry={nodes.Mark01.geometry}
-            material={materials.White}
-        />
-        <mesh
-            geometry={nodes.Hole03.geometry}
-            material={materials.Hole}
-            rotation={[0, 0, -Math.PI / 2]}
-        />
-        </group>
+        <></>
     );
 }
 
-useGLTF.preload("/blocks/block01.glb");
+useGLTF.preload("/blocks/Block01.glb");
