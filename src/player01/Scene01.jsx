535d4240f4e56f29da8df733e648be0106251a5b
NadiaArhbal <54160@etu.he2b.be>
2023-12-31 18:49:14 +0100

    :sparkles: Fix color problem

--- src/player01/Scene01.jsx
+++ src/player01/Scene01.jsx
@@ -3,6 +3,8 @@ import { OrbitControls, useGLTF, Stage } from "@react-three/drei"
 
 export default function Scene01({game}) {
     const model = useGLTF("/blocks/Mark01.glb");
+	const block = useGLTF("/blocks/Block01.glb");
+	const details = useGLTF("/blocks/Hole01.glb");
 
 	function onAccept(){
 		Rune.actions.accept();
@@ -16,18 +18,11 @@ export default function Scene01({game}) {
 
 	return (
 		<>
-			<div>
-				<span>{game.success ? "Succès :) " : "Erreur :( "}</span>
-				<span>{}</span>
-			</div>
-
-			<div>
-				<span>{game.solution ? "ACCEPT" : "REJECT"}</span>
-			</div>
-
 			<Canvas shadows camera={{ zoom: 0.15, fov: 20 }}>
 				<Stage adjustCamera={false} shadows={false} preset="rembrandt" intensity={1}  environment="forest">
 					<primitive object={model.scene} position={[0,0,0]}></primitive>
+					<primitive object={block.scene} position={[0,0,0]}></primitive>
+					<primitive object={details.scene} position={[0,0,0]}></primitive>
 				</Stage>
 				<OrbitControls enablePan={false} enableZoom={false}/>
 			</Canvas>
